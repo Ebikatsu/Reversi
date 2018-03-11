af1952f9721cd64fb9a170acdd961977757b4c0b
Naoki Watanabe <Ebikatsu@users.noreply.github.com>
2018-03-11 23:04:27 +0900

    勝利判定まで完成
    置ける場所を視覚化

--- proto2/Main.java
+++ proto2/Main.java
@@ -1,12 +1,18 @@
 package reversi;
 
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Main extends Applet implements MouseListener {
 	Reversi rvs;
+	int px, py;
+	String msg;
+	boolean isPlaying;
 
 	/*
 	 * @see java.applet.Applet#init()
@@ -20,6 +26,10 @@ public class Main extends Applet implements MouseListener {
 		int height = rvs.start[1] * 2 + rvs.height * rvs.boardSize + 200;
 		setSize(width, height); // アップレットのサイズを指定
 		addMouseListener(this);
+		// addMouseMotionListener(this);
+		px = 0;
+		py = 0;
+		isPlaying = true;
 	}
 
 	/*
@@ -29,6 +39,7 @@ public class Main extends Applet implements MouseListener {
 	public void start() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.start();
+		rvs.initBoard();
 	}
 
 	/*
@@ -55,6 +66,38 @@ public class Main extends Applet implements MouseListener {
 	@Override
 	public void paint(Graphics g) {
 		// TODO 自動生成されたメソッド・スタブ
+		Dimension size = getSize();
+		// 背景色の設定
+		g.setColor(Color.white);
+		g.fillRect(0, 0, size.width - 1, size.height - 1);
+
+		showBoard(g); // 盤の表示
+
+		rvs.setDisks(g);
+		int textY = rvs.start[1] + rvs.height * rvs.boardSize + 50;
+		g.setColor(Color.white);
+		g.fillRect(rvs.start[0], textY, 200, 100);
+
+		g.setColor(Color.black);
+
+		Font fnt = new Font("SansSerif", Font.BOLD, 22);
+		g.setFont(fnt);
+		if (isPlaying) {
+			if (rvs.turn) {
+				msg = "黒の番です";
+				g.drawString(msg, rvs.start[0], textY);
+			} else {
+				msg = "白の番です";
+				g.drawString(msg, rvs.start[0], textY);
+			}
+		} else {
+			msg = rvs.getWinner();
+			g.drawString(msg, rvs.start[0], textY);
+			destroy();
+		}
+		g.setColor(Color.black);
+		String point = "黒 " + rvs.diskCounts[0] + " vs " + rvs.diskCounts[1] + " 白";
+		g.drawString(point, 200, textY);
 	}
 
 	/*
@@ -76,7 +119,6 @@ public class Main extends Applet implements MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		// TODO 自動生成されたメソッド・スタブ
 		int x = e.getX();
 		int y = e.getY();
 		boolean onBoard = x > rvs.start[0] && x < rvs.start[0] + rvs.width * rvs.boardSize && y > rvs.start[1]
@@ -84,11 +126,18 @@ public class Main extends Applet implements MouseListener {
 		if (onBoard) {
 			int i = (int) ((x - rvs.start[0]) / rvs.width);
 			int j = (int) ((y - rvs.start[1]) / rvs.height);
-			if (rvs.enablePlace(i, j)) {
+			if (rvs.enablePlace(i, j, rvs.turn)) {
 				rvs.reverseDisks(i, j);
 				rvs.turn = rvs.turn ? false : true;
+				if (!rvs.enablePlace(rvs.turn)) {
+					rvs.turn = rvs.turn ? false : true;
+					if (!rvs.enablePlace(rvs.turn)) {
+						isPlaying = false;
+					}
+				}
 			}
 		}
+		rvs.countDisk();
 
 		repaint();
 	}
@@ -117,4 +166,26 @@ public class Main extends Applet implements MouseListener {
 
 	}
 
+	public void showBoard(Graphics g) {
+		int width = rvs.width;
+		int height = rvs.height;
+		g.setColor(Color.green);
+		g.fillRect(rvs.start[0], rvs.start[1], width * rvs.boardSize, height * rvs.boardSize);
+		Color lightgreen = new Color(173, 255, 47);
+		for (int i = 0; i < rvs.boardSize; i++) {
+			for (int j = 0; j < rvs.boardSize; j++) {
+				int x0 = rvs.start[0] + width * i;
+				int y0 = rvs.start[1] + height * j;
+				if (rvs.enablePlace(i, j, rvs.turn)) {
+					g.setColor(Color.red);
+					g.drawRect(x0, y0, width - 1, height - 1);
+					g.setColor(lightgreen);
+					g.fillRect(x0 + 1, y0 + 1, width - 2, height - 2);
+				} else {
+					g.setColor(Color.black);
+					g.drawRect(x0, y0, width - 1, height - 1);
+				}
+			}
+		}
+	}
 }
